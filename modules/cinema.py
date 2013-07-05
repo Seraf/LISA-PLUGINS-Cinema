af23ac596b84b480ce76eefe733e3def93837827
Seraf <julien.syx@gmail.com>
2013-07-05 17:18:08 +0200

    Adding translations

--- modules/cinema.py
+++ modules/cinema.py
@@ -8,7 +8,7 @@ import gettext
 
 path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
     inspect.getfile(inspect.currentframe()))[0],os.path.normpath("../lang/"))))
-_ = translation = gettext.translation(domain='chat', localedir=path, languages=[configuration['lang']]).ugettext
+_ = translation = gettext.translation(domain='cinema', localedir=path, languages=[configuration['lang']]).ugettext
 
 class Cinema:
     def __init__(self):
