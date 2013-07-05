5b41bda6c0ee691a383447bd964453b0e114780f
Seraf <julien.syx@gmail.com>
2013-07-05 15:30:39 +0200

    Adding translations for the plugin

--- lang/fr/cinema.rs
+++ lang/fr/cinema.rs
@@ -6,7 +6,7 @@
 > object getfilms python
 import sys, os, inspect
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split( \
-    inspect.getfile(inspect.currentframe()))[0],os.path.normpath("Plugins/Cinema/lang/fr/modules/"))))
+    inspect.getfile(inspect.currentframe()))[0],os.path.normpath("Plugins/Cinema/modules/"))))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
--- modules/cinema.py
+++ modules/cinema.py
@@ -1,13 +1,19 @@
 # -*- coding: UTF-8 -*-
-import urllib, json
+import urllib, json, os, inspect
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from lisa import configuration
 
+import gettext
+
+path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+    inspect.getfile(inspect.currentframe()))[0],os.path.normpath("../lang/"))))
+_ = translation = gettext.translation(domain='chat', localedir=path, languages=[configuration['lang']]).ugettext
+
 class Cinema:
     def __init__(self):
         self.configuration_lisa = configuration
-        mongo = MongoClient(self.configuration_lisa['database']['server'], \
+        mongo = MongoClient(self.configuration_lisa['database']['server'],
                             self.configuration_lisa['database']['port'])
         self.configuration = mongo.lisa.plugins.find_one({"name": "Cinema"})
 
@@ -15,10 +21,10 @@ class Cinema:
         film_str = ""
         for salle in self.configuration['configuration']['salles']:
             if salle['enabled'] == 'True':
-                film_str += u" Dans la salle "+ salle['name'] +u" sont joués les films : "
+                film_str += _('In the theater %s are played these films: ') % salle['name']
                 #lxml improve speed but need to be installed
                 #soup = BeautifulSoup(urllib.urlopen(configuration['url_' + salle['type']] + salle['id']),"lxml")
                 soup = BeautifulSoup(urllib.urlopen(self.configuration['configuration']['url_' + salle['type']] + salle['id']))
                 if salle['type'] == "Gaumont":
-                    film_str += u' puis '.join(unicode(film.get_text()) for film in soup.find_all("p", class_="titre"))
+                    film_str += _(' then ').join(unicode(film.get_text()) for film in soup.find_all("p", class_="titre"))
         return json.dumps({"plugin": "Cinema", "method": "getFilms", "body": film_str})
