fc0736f702a5c0380c581c07eb36582aecc63b20
Seraf <julien.syx@gmail.com>
2013-07-06 18:00:25 +0200

    Changing the way the module is loaded

--- lang/fr/cinema.rs
+++ lang/fr/cinema.rs
@@ -4,12 +4,6 @@
 + il ya quoi au cinéma [*]
 @ il y a quoi au cinema [*]
 > object getfilms python
-import sys, os, inspect
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split( \
-    inspect.getfile(inspect.currentframe()))[0],os.path.normpath("Plugins/Cinema/modules/"))))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
-
-from cinema import Cinema
+from Cinema.modules.cinema import Cinema
 return Cinema().getFilms()
 < object
