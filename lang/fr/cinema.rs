2e7c946dc291fbbec7c8d8876546acc983a674df
Seraf <julien.syx@gmail.com>
2013-08-12 16:49:40 +0200

    Adding WIT

--- lang/fr/cinema.rs
+++
@@ -1,9 +0,0 @@
-+ il y a quoi au cinema [*]
-- <call>getfilms</call>
-
-+ il ya quoi au cinéma [*]
-@ il y a quoi au cinema [*]
-> object getfilms python
-from Cinema.modules.cinema import Cinema
-return Cinema().getFilms()
-< object
